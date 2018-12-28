29fb0037115931796986b7504b04dd44fb2430bd
Sean T <teo259@gmail.com>
2018-12-29 02:06:15 +0800

    Add auth page for edit profile gui

---
+++ Patient/EditProfile_Auth.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Patient_EditProfile_Auth : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //TODO retrieve the security qns and answer
+        //TODO generate 2 random numbers
+        //TODO set the security questions to the labels
+
+    }
+
+    protected void SubmitBtn_Click(object sender, EventArgs e)
+    {
+
+    }
+}
