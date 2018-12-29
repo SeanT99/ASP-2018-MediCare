88ad487eaadd6cae3b5195448d39f7344524e133
Sean T <teo259@gmail.com>
2018-12-29 15:23:23 +0800

    Added Random number generator
    Still have bug, it doesnt print out the questions

--- Patient/EditProfile_Auth.aspx.cs
+++ Patient/EditProfile_Auth.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Web;
@@ -15,31 +16,31 @@ public partial class Patient_EditProfile_Auth : System.Web.UI.Page
         //retrieve the security qns and answer
         q.SecurityQuestionGet("ADMIN"); //TODO change to the current user
         //TODO generate 2 random numbers
-        int[] ran = ranNum();
+        string[] ran = ranNum();
 
         //set the security questions to the labels
-        if (ran[0] == 1)
+        if (ran[0] == "1")
         {
             question1 = q.Sec_qn1;
         }
-        else if (ran[0] == 2)
+        else if (ran[0] == "2")
         {
             question1 = q.Sec_qn2;
         }
-        else if (ran[0] == 3)
+        else if (ran[0] == "3")
         {
             question1 = q.Sec_qn3;
         }
 
-        if (ran[1] == 1)
+        if (ran[1] == "1")
         {
             question2 = q.Sec_qn1;
         }
-        else if (ran[1] == 2)
+        else if (ran[1] == "2")
         {
             question2 = q.Sec_qn2;
         }
-        else if (ran[1] == 3)
+        else if (ran[1] == "3")
         {
             question2 = q.Sec_qn3;
         }
@@ -54,6 +55,7 @@ public partial class Patient_EditProfile_Auth : System.Web.UI.Page
         //TODO check ans
     }
 
+    /*
     public int[] ranNum()
     {
         int length = 1;
@@ -72,5 +74,37 @@ public partial class Patient_EditProfile_Auth : System.Web.UI.Page
         int[] num = new int[] { a, b };
         return num;
     }
+    */
 
+
+    public string[] ranNum()
+    {
+        //get a value
+        string valid = "123";
+        string a = ran(valid);
+        //remove a from valid
+        if (a == "1")
+            valid = "23";
+        else if (a == "2")
+            valid = "13";
+        else if (a == "3")
+            valid = "12";
+        //get b value
+        string b = ran(valid);
+
+        Debug.Write(a + b);
+        
+        string[] result = new string[] { a, b };
+        return result;
+    }
+
+    public string ran(string valid)
+    {
+        StringBuilder res = new StringBuilder();
+        Random rnd = new Random();
+        
+        res.Append(valid[rnd.Next(valid.Length)]);
+        
+        return res.ToString();
+    }
 }
